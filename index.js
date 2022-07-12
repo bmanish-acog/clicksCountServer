685f4c4af541167cdacb195557fe4ab25edc020a
Manish Bajagai <bmanish@aganitha.ai>
2022-07-12 12:53:16 +0530

    Made new changes

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
-
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
