6cff66d7b2a753dbb56fb4647ec45afee4aecbec
Manish Bajagai <bmanish@aganitha.ai>
2022-07-12 16:01:55 +0530

    Cors code changes

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const app = express();
 
 const corsOptions ={
    origin:'*',
-   credentials:true,        
+   credentials:true,
    optionSuccessStatus:200,
 }
 
 app.use(cors(corsOptions))
-app.use(express.json());
+app.use(express.json({ strict: false }));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
